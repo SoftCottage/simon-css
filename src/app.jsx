0b93f6eb78354482827f8f450efd6fef243f28d4
SoftCottage <114368794+SoftCottage@users.noreply.github.com>
2025-12-15 16:10:07 -0700

    jsx files created

--- src/app.jsx
+++ src/app.jsx
@@ -47,7 +47,7 @@ export default function App() {
 
       <footer className="bg-dark text-white-50">
         <div className="container-fluid">
-          <span className="text-reset">Author Name(s)</span>
+          <span className="text-reset">Nerd</span>
           <a className="text-reset" href="https://github.com/webprogramming260/simon-css">
             Source
           </a>
